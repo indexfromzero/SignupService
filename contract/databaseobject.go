c822312c6383d7b187978d6a6327a26564fbe4b1
DevonKS <devonsteenberg@hotmail.com>
2016-03-16 08:55:47 +1300

    Made crud methods of the dataobject exportable

--- contract/databaseobject.go
+++ contract/databaseobject.go
@@ -5,14 +5,14 @@ import "github.com/microbusinesses/Micro-Businesses-Core/system"
 type IDatabaseObject interface {
 
 	//Creates a new object
-	create(object interface{}) error
+	Create(object interface{}) error
 
 	//Gets an existing object
-	get(id system.UUID) interface{}
+	Get(id system.UUID) interface{}
 
 	//Updates an existing object
-	update(id system.UUID, object interface{}) error
+	Update(id system.UUID, object interface{}) error
 
 	//Deletes an existing object
-	delete(id system.UUID) error
+	Delete(id system.UUID) error
 }
--- service/signupservice.go
+++ service/signupservice.go
@@ -4,18 +4,18 @@ import "github.com/microbusinesses/Micro-Businesses-Core/system"
 
 type SignupService struct{}
 
-func (SignupService) create(object interface{}) error {
+func (SignupService) Create(object interface{}) error {
 	return nil
 }
 
-func (SignupService) get(id system.UUID) interface{} {
+func (SignupService) Get(id system.UUID) interface{} {
 	return nil
 }
 
-func (SignupService) update(id system.UUID, object interface{}) error {
+func (SignupService) Update(id system.UUID, object interface{}) error {
 	return nil
 }
 
-func (SignupService) delete(id system.UUID) error {
+func (SignupService) Delete(id system.UUID) error {
 	return nil
 }
