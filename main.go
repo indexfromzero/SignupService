bea33296ec5e486c00af1d815312f125845889a6
DevonKS <devonsteenberg@hotmail.com>
2016-03-21 20:39:50 +1300

    Added create user handler and server shell

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/microbusinesses/Micro-Businesses-Core/system"
 	"github.com/microbusinesses/SignupService/domain"
 	"github.com/microbusinesses/SignupService/service"
@@ -54,4 +59,31 @@ func makeCreateUserEndpoint(svc service.SignupService) endpoint.Endpoint {
 		}
 		return createUserResponse{userId, ""}, nil
 	}
+}
+
+func main() {
+	ctx := context.Background()
+	svc := service.SignupService{}
+
+	createUserHandler := httptransport.NewServer(
+		ctx,
+		makeCreateUserEndpoint(svc),
+		decodeCreateUserRequest,
+		encodeCreateUserResponse,
+	)
+
+	http.Handle("/create-user", createUserHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeCreateUserRequest(r *http.Request) (interface{}, error) {
+	var request createUserRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, Err
+	}
+	return request, nil
+}
+
+func encodeCreateUserResponse(w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
 }
